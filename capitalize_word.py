f0d920b098ef03f930a69d9dda34c75171d9cb33
isfandiyor <isomiddinov025@gmail.com>
2024-12-18 22:01:41 +0500

    Delete capitalize_word.py

--- capitalize_word.py
+++
@@ -1,9 +0,0 @@
-def capitalize_word(word):
-    if len(word) == 0:
-        return word  
-    return word[0].upper() + word[1:]
-
-
-print(capitalize_word("hello"))  
-print(capitalize_word("Hello")) 
-print(capitalize_word("a"))      
